c14754876eaf35ee0fa78b7b595dec0b8526b753
Tosh Velaga <38474161+SanthoshVelaga@users.noreply.github.com>
2018-12-22 13:02:52 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function genQuote() {
-  var randNum = Math.floor(Math.random() * 8) + 1;
+  var randNum = Math.floor(Math.random() * 10) + 1;
   document.getElementById('quote').innerHTML = quotes[randNum];
   var tweetQuote = quotes[randNum].split(' ').join('%20');
   tweetQuote = tweetQuote.split('<br>').join('');
@@ -8,7 +8,7 @@ function genQuote() {
 }
 
 //quote array
-var quotes = ["Blank", "VR for dogs – take your dog outside without actually having to take him outside", "T shirt company that makes shirts for failed companies such as Webvan, Enron, and Pets.com", "AI for homework", "Publicly traded soundcloud rappers. Invest in the upwards trajectory of such legends as Teka$hi 6ix9ine", "Kale beer. It's good for you", "App that uses AI to help women avoid swiping right on guys who send dick pics", "Teka$hi 6ix9ine alarm clock",];
+var quotes = ["Blank", "VR for dogs – take your dog outside without actually having to take him outside", "T shirt company that makes shirts for failed companies such as Webvan, Enron, and Pets.com", "AI for twerking", "Publicly traded soundcloud rappers. Invest in 21 Savage", "Kale beer", "App that uses AI to help women avoid swiping right on guys who send dick pics", "Teka$hi 6ix9ine alarm clock that yells scum gang", "App that automatically sends your dad a tie he'll never wear for father's day", "social network for people who wear anti social social club", "drone bongs", ];
 
 
 
